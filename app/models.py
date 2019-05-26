6155e08e6f1f228f54a68ee4bf2dfd7cbc85e4fb
gichimux <gichimumwai1@gmail.com>
2019-05-27 02:34:41 +0300

    created user model

---
+++ app/models.py
@@ -0,0 +1,18 @@
+class User(UserMixin,db.Model):
+    '''
+    user class that creates instances of user object
+    '''
+
+    __tablename__='users'
+
+    #create the db columns
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship("Pitch", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="user", lazy = "dynamic")
